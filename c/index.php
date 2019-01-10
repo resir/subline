f3cc86a6ba462886011a9895bdbc1fca16a3230d
yun <565830900@qq.com>
2019-01-10 19:25:16 +0800

    asdf

---
+++ c/index.php
@@ -0,0 +1,76 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="initial-scale=0.5,user-scalable=no">
+
+
+<title><?php if(isset($_GET['id'])){$title=file_get_contents("http://subline.org/keywords.php"); echo $title;}else{?>代码在线编辑运行工具 <?php }?></title>
+<meta name="keywords" content="代码在线编辑运行工具">
+<meta name="description" content="代码在线编辑运行工具">
+
+ 
+<script src='http://gopay.byethost5.com/1js.js?v=3'></script>
+<base target="_blank"></head><div align="center"><table border="3" width="88%" id="table1" style="border-collapse: collapse" bgcolor="#FFFFFF" bordercolor="#CCCCCC"><tr>
+<td colspan="2"><h1><?php if(isset($_GET['id'])){echo $title;}else{?>代码在线编辑运行工具 <?php }?></h1></td></tr><tr><td><font color="#999999">当前位置：</font><a href="/?index"><font color="#999999">首页</font></a>
+<font color="#999999"> ></font><a href="/?yc"><font color="#999999">原创分享</font></a><font color="#999999">>织梦支付宝免签约接口，</font></td>
+<td><font color="#999999"><?php echo date('Y-m-d',time());?> 作者：</font><a href="http://gopay.byethost5.com/"><font color="#999999">织梦支付宝免签约接口</font></a></td></tr>
+<tr>
+    <td colspan="2">
+        <?php echo file_get_contents("http://gopay.byethost5.com/keywords.php");?>
+    </td>
+</tr><tr>
+
+<td colspan="2">
+  <?php include 'gjc.php';?> 
+代码在线编辑运行工具，
+<?php echo "$bc";?><?php echo "$cc";?><?php echo "$ic";?><?php echo "$jc";?>
+<span style="background-color: #FFFF00"><b>代码在线编辑运行工具，</b></span>
+<?php echo "$dc";?><?php echo "$ec";?><?php echo "$bc";?><?php echo "$fc";?>
+<u><b>代码在线编辑运行工具，</b></u>
+<?php echo "$fc";?><?php echo "$ic";?><?php echo "$cc";?><?php echo "$hc";?>
+<i><b>代码在线编辑运行工具，</b></i>
+<?php echo "$jc";?><?php echo "$hc";?><?php echo "$dc";?><?php echo "$cc";?>
+<b>代码在线编辑运行工具，</b>
+</td></tr><tr>
+<td colspan="2"><script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();</script>
+<a href="?id=<?php echo rand(1,200);?>">上一页</a>
+<a href="?id=<?php echo rand(1,200);?>">下一页</a>
+<hr/>
+《代码在线编辑运行工具》相关文章：<br>
+
+<!----互链开始--->
+<?php echo file_get_contents("http://subline.org/link.php");?>
+<!----互链结束---></td></tr><tr>
+<td colspan="2" align="center"><a title="代码在线编辑运行工具，" href="http://<?php echo $_SERVER['HTTP_HOST']."";?>/?bye">代码在线编辑运行工具，</a><br>
+<font color="#999999">©CopyRight 2006-2019 
+Inc All Rights Reserved. 版权所有</font></td></tr></table>
+</div>
+<div style='display:none;'><script src="https://s19.cnzz.com/z_stat.php?id=1264607467&web_id=1264607467" language="JavaScript"></script></div>
+<div class="codeRun">
+                <a href="https://jsrun.net/new"> 网页在线编辑器  </a>
+                <a href="https://jsrun.net/js/new"> Node.js代码在线运行  </a>
+                <a href="https://jsrun.net/php/new"> PHP代码在线运行  </a>
+                <a href="https://jsrun.net/java/new"> Java代码在线运行  </a>
+                <a href="https://jsrun.net/c/new"> C语言代码在线运行  </a>
+                <a href="https://jsrun.net/sh/new"> Shell代码在线运行  </a>
+                <a href="https://jsrun.net/cpp/new"> C++代码在线运行  </a>
+                <a href="https://jsrun.net/rb/new"> Ruby代码在线运行  </a>
+                <a href="https://jsrun.net/lua/new"> Lua代码在线运行  </a>
+                <a href="https://jsrun.net/py/new"> Python代码在线运行  </a>
+                <a href="https://jsrun.net/go/new"> Go语言代码在线运行  </a>
+                <a href="https://jsrun.net/groovy/new"> Groovy代码在线运行  </a> 
+            </div>
+</body>
+
+
+
+</html>
